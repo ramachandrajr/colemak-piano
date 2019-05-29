4ab6a0739f01f863f9e2dd74c8fe2ab3cfc3e229
Ajay Kumar <d.ajaykumar.017@gmail.com>
2019-05-29 15:29:14 +0530

    Change the order of notes to match colemak keyboard layout.

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,6 +1,12 @@
 document.onkeydown = function (n) {
-    var t = window.event ? n.keyCode : n.which;
-    n.shiftKey ? notes.b_c.indexOf(t) != -1 && piano.music("b" + t, 3) : notes.w_c.indexOf(t) != -1 && piano.music("a" + t, 1)
+    var t = window.event ? n.keyCode : n.which;    
+    if (n.shiftKey) {
+        if (notes.b_c.indexOf(t) !== -1)
+            piano.music("b" + t, 3);
+    } else {
+        if (notes.w_c.indexOf(t) !== -1)
+            piano.music("a" + t, 1);
+    }
 };
 document.onmouseup = function () {
     piano.isMouseDown = !1
